145d2768d763e517d79293a5e24a423d025c0d7e
Hnam140506 <nam1452000@gmail.com>
2026-07-12 21:38:23 -0700

    Revert "core"
    This reverts commit 39fbf5504f4f7dfb27526c06e54e96c12878ad5a.

--- obj/Debug/net9.0/WebBanVLXD.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebBanVLXD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanVLXD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29874e0a67ddd3039638a05c608feea8aab0b50c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddba63ed96f7913daf5713b6e9ec315483aaf3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanVLXD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanVLXD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
